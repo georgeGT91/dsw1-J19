f1343c8488b6c71df6ae8f3e991dd9e8cbc34788
Ernesto Anaya <ernestex@gmail.com>
2023-06-08 19:29:38 -0500

    aplicacion base cliente-soap-paises

---
+++ cliente-soap-paises/src/main/java/edu/idat/dsw1/clientesoappaises/ClienteSoapPaisesApplication.java
@@ -0,0 +1,13 @@
+package edu.idat.dsw1.clientesoappaises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteSoapPaisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteSoapPaisesApplication.class, args);
+	}
+
+}
---
+++ cliente-soap-paises/src/test/java/edu/idat/dsw1/clientesoappaises/ClienteSoapPaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.idat.dsw1.clientesoappaises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteSoapPaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
