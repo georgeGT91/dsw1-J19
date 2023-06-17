e69327a696e1b60747784eb44e1efc067062043a
georgeGT91 <mijalescanoJorge@gmail.com>
2023-06-17 01:35:54 -0500

    cambios2

---
+++ dsw1-soap-j20/src/main/java/com/example/dsw1soapj20/Dsw1SoapJ20Application.java
@@ -0,0 +1,13 @@
+package com.example.dsw1soapj20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1SoapJ20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1SoapJ20Application.class, args);
+	}
+
+}
---
+++ dsw1-soap-j20/src/test/java/com/example/dsw1soapj20/Dsw1SoapJ20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsw1soapj20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1SoapJ20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
